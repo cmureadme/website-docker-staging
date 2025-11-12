00360411cbb7e7c10b55ab7730f5a50cc7f0d0f6
Ryan Tosh <ryan.tosh@rto.run>
2025-11-12 17:38:37 -0500

    Fix staticfiles

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Collect static files
+python3 manage.py collectstatic --no-input --settings=readme_website.settings.staging
+
 # Migrate database
-python3 manage.py migrate
+python3 manage.py migrate --settings=readme_website.settings.staging
 
 # Start gunicorn
 DJANGO_SETTINGS_MODULE='readme_website.settings.staging' gunicorn --bind 0.0.0.0:8000 readme_website.wsgi
